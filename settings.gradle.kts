198b1644493b1010788ed508f91ef13494bb3402
Issao Hanaoka Junior <issaojr@gmail.com>
2024-03-02 13:42:35 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HyperFocus 15 Puzzle"
+include(":app")
