fc0277e4d349579a186935f647580c0ac83c83d1
Issao Hanaoka Junior <issaojr@gmail.com>
2024-03-03 16:45:11 -0300

    Minimum Viable Product. Game logic is completed and functional

--- app/src/main/java/com/issaojr/hyperfocus15puzzle/MainActivity.kt
+++ app/src/main/java/com/issaojr/hyperfocus15puzzle/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.issaojr.hyperfocus15puzzle
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -13,7 +15,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.AlertDialog
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -32,14 +39,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PuzzleBoard() {
-    val puzzleState = PuzzleState(4)
+    val puzzleState = remember {PuzzleState(4)}
     val gridSize = puzzleState.gridSize
+    val showDialog = remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(4.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(text = "Movimentos: ${puzzleState.movesCounter}", fontSize = 20.sp)
         for (i in 0 until gridSize) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -47,7 +56,7 @@ fun PuzzleBoard() {
             ) {
                 for (j in 0 until gridSize) {
                     val index = i * gridSize + j
-                    TileView(puzzleState.tiles[index].number)
+                    TileView(puzzleState, index)
                 }
             }
         }
@@ -56,11 +65,21 @@ fun PuzzleBoard() {
 
 
 @Composable
-fun RowScope.TileView(number: Int) {
+fun RowScope.TileView(puzzleState: PuzzleState, index: Int) {
+    val context = LocalContext.current
+    val number = puzzleState.tiles[index].number
+    val showDialog = remember { mutableStateOf(false) }
 
     Button(
         onClick = {
-
+            if (puzzleState.canMove(index)) {
+                puzzleState.move(index)
+                Toast.makeText(context, "Tile $number moved", Toast.LENGTH_SHORT).show()
+                puzzleState.incrementMovesCounter()
+                if(puzzleState.isGameFinished()) {
+                    showDialog.value = true
+                }
+            }
         },
         modifier = Modifier
             .aspectRatio(1f)
@@ -71,5 +90,31 @@ fun RowScope.TileView(number: Int) {
         if (number != 0) {
             Text(number.toString())
         }
+    }
+
+    if(showDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                showDialog.value = false
+                puzzleState.reset()
+            },
+            title = { Text("Parabéns!") },
+            text = { Text("Você finalizou o jogo!\nMovimentos: ${puzzleState.movesCounter}") },
+            confirmButton = {
+                Button(onClick = {
+                /* código para recomeçar o jogo */
+                    showDialog.value = false
+                    puzzleState.reset()
+                }) {
+                    Text("Recomeçar")
+                }
+            },
+            dismissButton = {
+                Button(onClick = { (context as Activity).finishAffinity() }) {
+                    Text("Sair")
+                }
+            },
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
--- app/src/main/java/com/issaojr/hyperfocus15puzzle/PuzzleState.kt
+++ app/src/main/java/com/issaojr/hyperfocus15puzzle/PuzzleState.kt
@@ -1,27 +1,61 @@
 package com.issaojr.hyperfocus15puzzle
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import kotlin.random.Random
 
 class PuzzleState(val gridSize: Int) {
-    val tiles = mutableStateListOf(*List(gridSize * gridSize) { Tile(it) }.toTypedArray())
+    private val initialTiles = List(gridSize * gridSize - 1) { Tile(it + 1) } + Tile(0)
+    val tiles = mutableStateListOf(*List(gridSize * gridSize - 1) { Tile(it + 1) }.toTypedArray(), Tile(0))
+    var movesCounter: Int by mutableIntStateOf(0)
+
 
     init {
         shuffle()
     }
 
     private fun shuffle() {
-        val random = Random.Default
+
         repeat(300) { // Make 300 random moves to shuffle the tiles
-            val emptyTileIndex = tiles.indexOfFirst { it.isEmpty }
-            val direction = listOf(-1, 1, -gridSize, gridSize)
-                .filterNot { emptyTileIndex % gridSize == 0 && it == -1 }
-                .filterNot { emptyTileIndex % gridSize == gridSize - 1 && it == 1 }
-                .random(random)
-            val otherTileIndex = emptyTileIndex + direction
-            if (otherTileIndex in 0 until gridSize * gridSize) {
-                tiles[emptyTileIndex] = tiles[otherTileIndex].also { tiles[otherTileIndex] = tiles[emptyTileIndex] }
+            while (true) {
+                val index = (0..15).random()
+                if(canMove(index)) {
+                    move(index)
+                    break
+                }
             }
         }
+    }
+
+    public fun canMove(index: Int): Boolean {
+        val emptyTileIndex = tiles.indexOfFirst { it.isEmpty }
+
+        return (index == emptyTileIndex - 1 && index % gridSize != gridSize - 1) || // à esquerda
+                (index == emptyTileIndex + 1 && index % gridSize != 0) || // à direita
+                index == emptyTileIndex - gridSize || // acima
+                index == emptyTileIndex + gridSize // abaixo
+    }
+
+    public fun move(index: Int){
+        val emptyTileIndex = tiles.indexOfFirst { it.isEmpty }
+        tiles[emptyTileIndex] = tiles[index].also { tiles[index] = tiles[emptyTileIndex] }
+    }
+
+    public fun isGameFinished(): Boolean {
+        return tiles.zip(initialTiles).all { (tile, initialTile) -> tile.number == initialTile.number }
+    }
+
+    public fun incrementMovesCounter() {
+        movesCounter++
+    }
+
+    fun reset() {
+        tiles.clear()
+        tiles.addAll(List(gridSize * gridSize - 1) { Tile(it + 1) } + Tile(0))
+        shuffle()
+        movesCounter = 0
     }
 }
